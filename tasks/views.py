125bd60d9d3af59414b30ff987f2c57b5c9bba13
J2kub <kubogloncak@gmail.com>
2026-04-14 22:16:36 +0200

    Display tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Task
 
 
 def home(request):
+    tasks = Task.objects.all().order_by('-created_at')
+
     context = {
         'app_name': 'FamilyToDo',
         'message': 'Welcome to FamilyToDo',
