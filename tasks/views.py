b8adae10e674b72072ad46eb13f9a00d98217621
J2kub <kubogloncak@gmail.com>
2026-04-14 22:22:38 +0200

    Display tasks fix

--- tasks/views.py
+++ tasks/views.py
@@ -6,9 +6,9 @@ def home(request):
     tasks = Task.objects.all().order_by('-created_at')
 
     context = {
-        'app_name': 'FamilyToDo',
-        'message': 'Welcome to FamilyToDo',
-        'tasks': ['Task 1', 'Task 2', 'Task 3'],
+        'app_name': 'Family Todo',
+        'message': f'Pocet taskov v databaze: {tasks.count()}',
+        'tasks': tasks,
     }
-    return render(request, 'tasks/home.html', context)
 
+    return render(request, 'tasks/home.html', context)
