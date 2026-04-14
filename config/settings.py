2dea054e01b131e25334b8858431c6a5bb9b15b7
J2kub <kubogloncak@gmail.com>
2026-04-14 23:12:27 +0200

    Logout fix

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/accounts/login/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
